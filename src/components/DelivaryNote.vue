6f9ac3b84c059f1168319e9370d3fb1ab6477290
Mohammad L Abonjem <mabonjem@stcs.com.sa>
2020-02-23 18:21:10 +0300

    Comment

---
+++ src/components/DelivaryNote.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="DNForm">
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Project</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>PO</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Customer PO</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>Delivered by</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+     <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Delivered Date</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>Short Description</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+
+       
+       <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+
+
+     
+     <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Customer Name</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>Signature</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Contact Person</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>Telephone No</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Email Address</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>FAX No</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>Bldg. Address</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>TEL </label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+    <div class="form-row">
+      <div class="form-group col-md-6">
+        <label>City</label>
+        <input type="email" class="form-control" id="inputEmail4" placeholder="Email" />
+      </div>
+
+      <div class="form-group col-md-6">
+        <label>Remarks</label>
+        <input type="password" class="form-control" id="inputPassword4" placeholder="Password" />
+      </div>
+    </div>
+
+
+  </div>
+</template>
